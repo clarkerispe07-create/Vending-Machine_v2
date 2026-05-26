01d090d772f95ae768e87cd5a89242d95eaf34a2
Clark Dee <263336227+clarkerispe07-create@users.noreply.github.com>
2026-05-26 15:16:02 +0800

    Add files via upload

--- file.c
+++ file.c
@@ -21,19 +21,20 @@ char *liquorStore[5] = {
 };
 float liquorPrices[5] = {150, 150, 200, 250, 120};
 
-int softStock[5] = {50, 65, 75, 80, 30};
+int softStock[5] = {50, 100, 75, 80, 30};
 int junkStock[5] = {56, 100, 70, 44, 200};
-int waterStock[5] = {20, 15, 10, 8, 12};
+int waterStock[5] = {20, 15, 10, 80, 12};
 int liquorStock[5] = {30, 50, 20, 50, 40};
 
 void displayStores() {
-    printf("==========================================\n");
+    printf("\n==========================================\n");
     printf("               SELECT STORE            \n");
     printf("===========================================\n");
-    printf("1. Soft Drinks\n");
-    printf("2. Junk Foods\n");
-    printf("3. Water\n");
+    printf("1. Soft Drinks Store\n");
+    printf("2. Junk Foods Store\n");
+    printf("3. Water Store\n");
     printf("4. Liquor Store\n");
+    printf("5. Exit\n");
     printf("===========================================\n");
 }
 
@@ -64,10 +65,33 @@ void printReceipt(char *product, int quantity, float total, float money, float c
     printf("Cash    : PHP %.2f\n", money);
     printf("Change  : PHP %.2f\n", change);
     printf("================================\n");
-    printf("Thank you for your purchase!\n");
+    printf("Thank you for your purchase!\n\n");
     saveToFile(product, quantity, total, money, change);
 }
 
+void saveStock() {
+    FILE *file = fopen("stock.txt", "w");
+
+    if (file == NULL) {
+        printf("Error saving stock!\n");
+        return;
+    }
+
+    int *stocks[] = { softStock, junkStock, waterStock, liquorStock };
+    char *names[] = { "Soft", "Junk", "Water", "Liquor" };
+
+    for (int i = 0; i < 4; i++) {
+        fprintf(file, "%s: ", names[i]);
+
+        for (int j = 0; j < 5; j++) {
+            fprintf(file, "%d ", stocks[i][j]);
+        }
+
+        fprintf(file, "\n");
+    }
+
+    fclose(file);
+}
 
 int main() {
 
@@ -79,6 +103,7 @@ int main() {
     float total, change;
     float pocketMoney = 500.00;
     char *product;
+    char buyAgain;
 
 storeMenu:
 
@@ -86,8 +111,15 @@ storeMenu:
 
         displayStores();
 
-        printf("Enter store number: ");
-        scanf("%d", &store);
+       printf("Enter store number: ");
+
+        if (scanf("%d", &store) != 1) {
+             printf("\nInvalid input! Numbers only.\n");
+
+            while (getchar() != '\n');
+
+        continue;
+        }
 
         if (store < 1 || store > 4) {
             printf("Exiting program...\n");
@@ -137,7 +169,7 @@ productMenu:
 
             printf("0. Back to Store Menu\n");
 
-            printf("Enter product number: ");
+            printf("\nEnter product number: ");
             scanf("%d", &choice);
 
             if (choice == 0) goto storeMenu;
@@ -147,10 +179,25 @@ productMenu:
                 continue;
             }
 
-            if (stock[choice - 1] == 0) {
-                printf("OUT OF STOCK!\n");
-                continue;
-            }
+
+                if (stock[choice - 1] == 0) {
+                    int option;
+
+                        printf("\nTHE PRODUCT IS OUT OF STOCK!\n");
+                        printf("0. Cancel\n");
+                        printf("1. Choose another product\n");
+                        printf("2. Back to Store Menu\n");
+                        printf("Enter choice: ");
+                        scanf("%d", &option);
+
+                        if (option == 1) {
+                                goto productMenu;
+                        } else if(option == 2){
+                                goto storeMenu;
+                    } else {
+                         return 0;   
+                    }
+                }   
 
 quantityMenu:
 
@@ -174,29 +221,42 @@ quantityMenu:
                 total = prices[choice - 1] * quantity;
 
                 if (pocketMoney < total) {
-                    printf("Insufficient money! Need PHP %.2f more\n", total - pocketMoney);
-                    return 0;
+                    printf("\nInsufficient money! Need PHP %.2f more\n", total - pocketMoney);
+                    printf("Choose again!\n\n");
+                   goto storeMenu;
                 }
 
                 float oldMoney = pocketMoney;
 
                 product = items[choice - 1];
 
-                printf("\nPrevious Money: PHP %.2f\n", oldMoney);
-                printf("You Buy %s (%d pcs, PHP %.2f)\n", product, quantity, total);
+                printf("\n===========================================");
+                printf("\n       Previous Money: PHP %.2f\n", oldMoney);
+                printf("   You Buy %s (%d pcs, PHP %.2f)\n", product, quantity, total);
+                printf("===========================================");
+
 
                 pocketMoney -= total;
                 stock[choice - 1] -= quantity;
+                saveStock();
 
                 float change = oldMoney - total;
 
-                printf("\nRemaining Money: PHP %.2f\n", pocketMoney);
-
-                printf("Remaining Stock: %s = %d pcs\n",product, stock[choice - 1]);
-
+                printf("\n       Remaining Money: PHP %.2f\n", pocketMoney);
+                printf("   Remaining Stock: %s = %d pcs\n",product, stock[choice - 1]);
+                printf("===========================================\n");
                 printReceipt(product, quantity, total, oldMoney, change);
 
-                return 0;
+               printf("\nDo you want to buy again? (Y/N):  ");
+                scanf(" %c", &buyAgain);
+
+                    if (buyAgain == 'Y' || buyAgain == 'y') {
+                                goto storeMenu;
+                    }
+                        else {
+                            printf("\nThank you for using the vending machine!\n");
+                                return 0;
+                }
             }
         }
     }
